75d25f467068abc4ac7bcfda63b4ed0234ec675b
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-12-22 21:46:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 function setup(){
     canvas= createCanvas(400,370);
     canvas.position(530,370);
+    video= createCapture(VIDEO);
+    video.hide();
+    classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/OlPNOG13M/model.json', modelLoaded);
+
+}
+
+function draw(){
+    image(video,0,0,400,370);
+    classifier.classify(video, gotResult);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error("error!");
+    }
+    else{
+        console.log(results);
+        document.getElementById("results").innerHTML= results[0].label;
+        document.getElementById("accuracy").innerHTML= results[0].confidence.toFixed(2);
+    }
 }
